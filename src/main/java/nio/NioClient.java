51311f5ca8c319be2b37ad01aa65079be267bdb6
liujunpeng <liujunpeng@sinovatech.com>
2021-07-21 18:08:18 +0800

    nio客户端和服务端基本使用

---
+++ src/main/java/nio/NioClient.java
@@ -0,0 +1,104 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @Description: nio实现客户端连接
+ * @Author: liujunpeng
+ * @Date: 2021/7/21 10:46
+ * @Version: 1.0
+ */
+public class NioClient {
+
+    private Selector selector;
+
+    /**
+     * 对selector和 socketChannel进行初始化操作
+     * 绑定端口
+     *
+     * @param ip
+     * @param port
+     */
+    public void initClinet(String ip, int port) throws IOException {
+        //获取scoketChannel
+        SocketChannel socketChannel = SocketChannel.open();
+        //设置socketChannel非阻塞
+        socketChannel.configureBlocking(false);
+        //获取selector
+        this.selector = Selector.open();
+        //连接ip和端口
+        socketChannel.connect(new InetSocketAddress(ip, port));
+        //注册连接事件
+        socketChannel.register(selector, SelectionKey.OP_CONNECT);
+    }
+
+    /**
+     * 使用轮训方式监听selector发生的事件
+     */
+    public void listen() throws IOException {
+        while (true) {
+            //没有事件发送就会阻塞在当前位置
+            selector.select(1000);
+            //获取发生的事件
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            //遍历
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                //判断是否连接事件发生
+                if (selectionKey.isConnectable()) {
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    //是否正在连接，如果正在连接则完成连接
+                    if (socketChannel.isConnectionPending()) {
+                        socketChannel.finishConnect();
+                    }
+                    //设置scoketChannel非阻塞
+                    socketChannel.configureBlocking(false);
+                    System.out.println("已成功连接服务端！！");
+                    //发送消息过去
+                    Scanner scanner = new Scanner(System.in);
+                    while (scanner.hasNextLine()) {
+                        String msg = scanner.nextLine().trim();
+                        socketChannel.write(ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8)));
+                    }
+                    //并且监听读取事件
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (selectionKey.isReadable()) {
+                    //接受读取事件
+                    read(selectionKey);
+                    System.out.println("clinet发生read事件");
+                }
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * selectionKey读取内容
+     *
+     * @param selectionKey
+     */
+    public void read(SelectionKey selectionKey) throws IOException {
+        //根据selectionKey获取socketChannel
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        //创建读取数据缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        //读取数据
+        socketChannel.read(byteBuffer);
+        System.out.println(new String(byteBuffer.array()));
+    }
+
+    public static void main(String[] args) throws IOException {
+        NioClient nioClient = new NioClient();
+        nioClient.initClinet("127.0.0.1", 6666);
+        nioClient.listen();
+    }
+}
--- src/main/java/nio/NioServer.java
+++ src/main/java/nio/NioServer.java
@@ -1,13 +1,101 @@
 package nio;
 
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
 /**
- * @Description: nio实现服务店监听
+ * @Description: nio实现服务端监听
  * @Author: liujunpeng
  * @Date: 2021/7/20 9:35
  * @Version: 1.0
  */
 public class NioServer {
-    public static void main(String[] args) {
 
+    /**
+     * selector
+     */
+    private Selector selector;
+
+    /**
+     * 初始化，并且绑定端口
+     */
+    public void initServer(int port) throws IOException {
+        //创建ServerScoketChannel服务通道
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //服务端绑定ip和端口
+        serverSocketChannel.bind(new InetSocketAddress(port));
+        //设置服务端通道为非阻塞
+        serverSocketChannel.configureBlocking(false);
+        //创建Selector路由
+        this.selector = Selector.open();
+        //把服务端通道注册到selector上，并且监听接受事件
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        System.out.println("当前selector注册数为：" + selector.keys().size());
+    }
+
+    /**
+     * 轮训方式，监听selector上是否有时间发生
+     */
+    public void listen() throws IOException {
+        while (true) {
+            //未发生事件时为阻塞状态
+            selector.select(1000);
+            //获取发生事件的SelectionKey
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                //判断是否是接受事件
+                if (selectionKey.isAcceptable()) {
+                    //获取事件的channel
+                    ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = channel.accept();
+                    //设置非阻塞
+                    socketChannel.configureBlocking(false);
+                    //向客户端发送消息
+                    socketChannel.write(ByteBuffer.wrap(("已经接收到：" + socketChannel.getRemoteAddress() + " 的连接！").getBytes(StandardCharsets.UTF_8)));
+                    System.out.println("已经接收到：" + socketChannel.getRemoteAddress() + " 的连接！");
+                    //并且监听客户端发送消息
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (selectionKey.isReadable()) {
+                    //读取消息
+                    read(selectionKey);
+                }
+                //删除当前selectionKey方式重复监听
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * 根据selectionKey读取消息
+     *
+     * @param selectionKey
+     * @throws IOException
+     */
+    public void read(SelectionKey selectionKey) throws IOException {
+        //获取监听的客户端channel
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        //创建读取数据的缓存区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        //读取数据
+        socketChannel.read(byteBuffer);
+        //接收到客户端发送数据
+        System.out.println(new String(byteBuffer.array()));
+        //回复客户端
+        socketChannel.write(ByteBuffer.wrap("已经成功收到消息".getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static void main(String[] args) throws IOException {
+        NioServer nioServer = new NioServer();
+        nioServer.initServer(6666);
+        nioServer.listen();
     }
 }
